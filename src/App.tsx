ba0f62f451c183b2ab72c316c1f27fa0cb99e0c0
NihaRuksar <146099981+NihaRuksar@users.noreply.github.com>
2026-07-10 11:27:12 +0530

    Add flex layout to main section in App component

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@
  * @license
  * SPDX-License-Identifier: Apache-2.0
  */
-
 import { Navbar } from './components/layout/Navbar';
 import { CustomCursor } from './components/layout/CustomCursor';
 import { PremiumBackground } from './components/layout/PremiumBackground';
@@ -12,7 +11,6 @@ import { AllProjectsAccordion } from './components/sections/AllProjectsAccordion
 import { TechStack } from './components/sections/TechStack';
 import { Contact } from './components/sections/Contact';
 import { Footer } from './components/layout/Footer';
-import { motion } from 'motion/react';
 
 export default function App() {
   return (
@@ -20,7 +18,7 @@ export default function App() {
       <PremiumBackground />
       <CustomCursor />
       <Navbar />
-      <main className="relative z-10">
+      <main className="relative z-10 flex flex-col">
         <Hero />
         <AllProjectsAccordion />
         <TechStack />
